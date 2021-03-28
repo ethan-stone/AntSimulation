7dd54265cffcbc4088c8db5442d73792af81330a
ethan-stone <ethan.stone9352@gmail.com>
2021-03-28 03:44:37 -0400

    added detection of food

--- Assets/Scripts/Ant.cs
+++ Assets/Scripts/Ant.cs
@@ -4,20 +4,27 @@ using UnityEngine;
 
 public class Ant : MonoBehaviour
 {
-    public float maxSpeed = 20f;
-    public float steerStrength = 4f;
-    public float wanderStrength = 1f;
+    public float maxSpeed = 2;
+    public float steerStrength = 2;
+    public float wanderStrength = 1;
+
+    public LayerMask foodLayer;
+
+    public float viewRadius;
+    public float viewAngle;
 
     Vector2 position;
     Vector2 velocity;
     Vector2 desiredDirection;
+
+    Transform targetFood;
 
     void Update() {
         desiredDirection = (desiredDirection + Random.insideUnitCircle * wanderStrength).normalized;
 
         Vector2 desiredVelocity = desiredDirection * maxSpeed;
         Vector2 desiredSteeringForce = (desiredVelocity - velocity) * steerStrength;
-        Vector2 acceleration = Vector2.ClampMagnitude(desiredDirection, steerStrength) / 1;
+        Vector2 acceleration = Vector2.ClampMagnitude(desiredSteeringForce, steerStrength) / 1;
 
         velocity = Vector2.ClampMagnitude(velocity + acceleration * Time.deltaTime, maxSpeed);
 
@@ -30,8 +37,42 @@ public class Ant : MonoBehaviour
         }
 
         position += velocity * Time.deltaTime;
+        HandleFood();
 
         float angle = Mathf.Atan2(velocity.y, velocity.x) * Mathf.Rad2Deg;
         transform.SetPositionAndRotation(position, Quaternion.Euler(0, 0, angle));
+    }
+
+    public Vector3 DirFromAngle(float angleInDegrees, bool angleIsGlobal) {
+        if (!angleIsGlobal) {
+            angleInDegrees += transform.eulerAngles.y;
+        }
+        return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+    }
+
+    void HandleFood() {
+        Collider[] allFood = Physics.OverlapSphere(transform.position, viewRadius, foodLayer);
+        
+        print(allFood.Length);
+        if (targetFood == null) {
+            if (allFood.Length > 0) {
+                Transform food = allFood[Random.Range(0, allFood.Length)].transform;
+                Vector2 dirToFood = (food.position - transform.position).normalized;
+
+                if (Vector2.Angle(transform.forward, dirToFood) < viewAngle / 2) {
+                    food.gameObject.layer = LayerMask.NameToLayer("TakenFood");
+                    targetFood = food;
+                }
+            }
+        } else {
+            desiredDirection = (targetFood.position - transform.position).normalized;
+            
+            const float foodPickupRadius = 0.5f;
+            if (Vector3.Distance(targetFood.position, transform.position) < foodPickupRadius) {
+                targetFood.position = transform.position;
+                targetFood.parent = transform;
+                targetFood = null;
+            }
+        }
     }
 }
---
+++ Assets/Scripts/FoodSpawner.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodSpawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject foodPrefab;
+    public int numFood;
+    public float spawnSize;
+
+    Vector2 screenHalfSizeInWorldUnits;
+
+    void Start() {
+        screenHalfSizeInWorldUnits = new Vector2(Camera.main.aspect * Camera.main.orthographicSize, Camera.main.orthographicSize);
+        for (int i = 0; i < numFood; i++) {
+            Vector2 spawnPosition = new Vector2(Random.Range(-screenHalfSizeInWorldUnits.x, screenHalfSizeInWorldUnits.x), Random.Range(-screenHalfSizeInWorldUnits.y, screenHalfSizeInWorldUnits.y));
+            GameObject newFood = (GameObject) Instantiate(foodPrefab, spawnPosition, Quaternion.Euler(Vector3.forward * 0));
+            newFood.transform.localScale = Vector2.one * spawnSize;
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
